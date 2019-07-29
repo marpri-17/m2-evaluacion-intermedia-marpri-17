d723515d9fce73ea36be7f44beea95f3c83d4b0c
Mar Prieto <mar.pnieto@gmail.com>
2019-07-29 16:12:01 +0200

    logic almost finish

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,50 @@
-'use strict';
+"use strict";
+// DOM Elements
+const btnReset = document.querySelector(".js-btnReset");
 
-console.log('>> Ready :)');
+const counter = document.querySelector(".js-score");
+
+const btnCheck = document.querySelector(".js-check");
+const inputNumber = document.querySelector(".js-input");
+const clue = document.querySelector(".clue");
+// Funciones
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+const reloadPage = () => location.reload();
+//Handler
+const checkNumber = ev => {
+  ev.preventDefault();
+  increaseAcc();
+  let userNumber = parseInt(inputNumber.value);
+  if (userNumber === randomNumber) {
+    clue.innerHTML = "Has ganado";
+    btnReset.classList.remove("hidden");
+    btnReset.classList.add("show");
+  } else if (userNumber < randomNumber) {
+    clue.innerHTML = "Más alto";
+  } else if (userNumber > randomNumber) {
+    clue.innerHTML = "Más bajo";
+  } else {
+    clue.innerHTML = "Introduce un número entero entre 0 y 100";
+  }
+};
+//Get ramdon number when page load
+const randomNumber = getRandomNumber(100);
+console.log(randomNumber);
+/* debugger; */
+
+// Counter
+let acc = 0;
+const increaseAcc = () => {
+  acc++;
+  counter.innerHTML = acc;
+  console.log(acc);
+};
+
+//Listeners
+
+btnCheck.addEventListener("click", checkNumber);
+
+btnReset.addEventListener("click", reloadPage);
