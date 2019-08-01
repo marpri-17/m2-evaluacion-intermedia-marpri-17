aa36a2fa3b61f2315eccd6bd8152be2cffc96488
Mar Prieto <mar.pnieto@gmail.com>
2019-08-02 01:34:04 +0200

    Fix issue and add PRICE :smirk_cat:

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -7,31 +7,42 @@ const counter = document.querySelector(".js-score");
 const btnCheck = document.querySelector(".js-check");
 const inputNumber = document.querySelector(".js-input");
 const clue = document.querySelector(".clue");
+const price = document.querySelector(".js-price");
 // Funciones
+const getPrice = () => {
+  fetch("https://cataas.com/cat/says/¡¡Soy tu premio  humano!!").then(
+    response => (price.src = response.url)
+  );
+};
 
+var setClue = hint => (clue.innerHTML = hint);
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 const reloadPage = () => location.reload();
 //Handler
-const checkNumber = ev => {
+const updatePage = function(ev) {
   ev.preventDefault();
   increaseAcc();
+  checkNumber();
+};
+const checkNumber = () => {
   let userNumber = parseInt(inputNumber.value);
   if (userNumber === randomNumber) {
-    clue.innerHTML = "¡¡FELICIDADES!! Has ganado,campeona";
-    btnReset.classList.remove("hidden");
-    btnReset.classList.add("show");
+    setClue("¡¡FELICIDADES!! Has ganado,campeona");
+    getPrice();
+    price.classList.toggle("hidden");
+    btnReset.classList.toggle("hidden");
   } else if (userNumber < randomNumber && userNumber > 0) {
-    clue.innerHTML = "Demasiado bajo. Prueba una cifra más alta";
+    setClue("Demasiado bajo. Prueba una cifra más alta");
   } else if (userNumber > randomNumber && userNumber < 100) {
-    clue.innerHTML = "Demasiado alto. Prueba una cifra más baja";
+    setClue("Demasiado alto. Prueba una cifra más baja");
   } else if (userNumber < 0) {
-    clue.innerHTML = "¡Este número es negativo!";
+    setClue("¡Este número es negativo!");
   } else if (userNumber > 100) {
-    clue.innerHTML = "¡El juego es hasta 100!";
+    setClue("¡El juego es hasta 100!");
   } else {
-    clue.innerHTML = "Introduce un número entero entre 0 y 100";
+    setClue("Introduce un número entero entre 0 y 100");
   }
 };
 //Get ramdon number when page load
@@ -49,6 +60,6 @@ const increaseAcc = () => {
 
 //Listeners
 
-btnCheck.addEventListener("click", checkNumber);
+btnCheck.addEventListener("click", updatePage);
 
 btnReset.addEventListener("click", reloadPage);
